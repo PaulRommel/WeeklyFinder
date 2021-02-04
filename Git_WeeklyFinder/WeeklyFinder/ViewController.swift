94df64303307a07167875b4c8e6b3d5aa55b6840
PaulRommel <paulrommel87@gmail.com>
2021-02-04 22:19:12 +0300

    Create Project
    Day 1

---
+++ Git_WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Павел Попов on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!    
+    @IBOutlet weak var resultLabel: UILabel!
+     
+    @IBAction func findDay(_ sender: UIButton) {
+    }
+    
+}
+
